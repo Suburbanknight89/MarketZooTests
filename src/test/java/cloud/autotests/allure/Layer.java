b12cce52f536ebac8b0196ca04a12d9061e6d10b
suburbanknight89 <lumenteam@gmail.com>
2021-11-02 21:26:08 +0300

    1 add tests and testbase

---
+++ src/test/java/cloud/autotests/allure/Layer.java
@@ -0,0 +1,15 @@
+package cloud.autotests.allure;
+
+
+import io.qameta.allure.LabelAnnotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@LabelAnnotation(name = "layer")
+public @interface Layer {
+	String value();
+}
